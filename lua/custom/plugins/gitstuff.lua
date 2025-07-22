1eae9edcf11aea4c529d8d61c6b0120b20452135
Kristian De Castro <22585924+OkayKris@users.noreply.github.com>
2025-07-22 02:49:18 -0700

    fixup! add git integrations

--- lua/custom/plugins/gitstuff.lua
+++ lua/custom/plugins/gitstuff.lua
@@ -1,3 +1,10 @@
 return {
-  'tpope/vim-fugitive',
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+      'ibhagwan/fzf-lua', -- optional
+    },
+  },
 }
---
+++ lua/custom/plugins/keymaps.lua
@@ -0,0 +1,3 @@
+return {
+  vim.keymap.set('n', '<leader>ng', ':Neogit', { noremap = true, silent = true }),
+}
